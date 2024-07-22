2f03c34503fec3278368f99178be6a6c26411b3c
Sokortov Ivan <sokortovi@gmail.com>
2024-07-22 11:34:18 +0200

    gfgh

--- install_fishstick.py
+++ install_fishstick.py
@@ -1,12 +1,15 @@
-import os
-import subprocess
-import zipfile
+import os, subprocess, zipfile
+from subprocess import run
 
-downloads_path = os.path.expanduser("~/Downloads")
-repo_url = "git@github.com:Eyelights/fishstick-blackband.git"
+FISHSTICK_PATH = os.path.expanduser("~/Downloads/fishstick-blackband")
+FISHSTICK_GIT_URL = "git@github.com:Eyelights/fishstick-blackband.git"
+S3_DEPENDENCIES_URLS = [
+    "s3://wqerwerwqer/create_file/296/DEBUG.zip",
+    "s3://wqerwerwqer/create_file/296/RELEASE.zip"
+]
 
 def get_branches(repo_url):
-    result = subprocess.run(["git", "ls-remote", "--heads", repo_url], capture_output=True, text=True)
+    result = run(["git", "ls-remote", "--heads", repo_url], capture_output=True, text=True)
     branches = []
     for line in result.stdout.splitlines():
         branch = line.split("/")[-1]
@@ -16,22 +19,22 @@ def get_branches(repo_url):
 def clone_or_update_repo(repo_url, branch, destination):
     if os.path.exists(destination):
         print(f"Repository already exists. Updating {destination}")
-        subprocess.run(["git", "checkout", branch], cwd=destination, check=True)
-        subprocess.run(["git", "pull", "origin", branch], cwd=destination, check=True)
+        run(["git", "checkout", branch], cwd=destination, check=True)
+        run(["git", "pull", "origin", branch], cwd=destination, check=True)
     else:
         print(f"Cloning repository to {destination}")
-        subprocess.run(["git", "clone", "--branch", branch, repo_url, destination], check=True)
-        subprocess.run(["git", "lfs", "install"], cwd=destination, check=True)
+        run(["git", "clone", "--branch", branch, repo_url, destination], check=True)
+        run(["git", "lfs", "install"], cwd=destination, check=True)
 
 def download_and_extract_s3_files(urls, destination):
     for url in urls:
         zip_file_name = os.path.join(destination, url.split('/')[-1])
-        subprocess.run(["aws", "s3", "cp", url, zip_file_name], check=True)
+        run(["aws", "s3", "cp", url, zip_file_name], check=True)
         with zipfile.ZipFile(zip_file_name, 'r') as zip_ref:
             zip_ref.extractall(destination)
         os.remove(zip_file_name)
 
-branches = get_branches(repo_url)
+branches = get_branches(FISHSTICK_GIT_URL)
 print("Available branches:")
 for i, branch in enumerate(branches):
     print(f"{i}: {branch}")
@@ -39,16 +42,8 @@ for i, branch in enumerate(branches):
 branch_index = int(input("Select branch to clone or update (number): "))
 selected_branch = branches[branch_index]
 
-repo_name = repo_url.split("/")[-1].replace(".git", "")
-clone_path = os.path.join(downloads_path, repo_name)
+clone_or_update_repo(FISHSTICK_GIT_URL, selected_branch, FISHSTICK_PATH)
+print(f"Repository cloned or updated to {FISHSTICK_PATH}")
 
-clone_or_update_repo(repo_url, selected_branch, clone_path)
-print(f"Repository cloned or updated to {clone_path}")
-
-s3_urls = [
-    "s3://wqerwerwqer/create_file/296/DEBUG.zip",
-    "s3://wqerwerwqer/create_file/296/RELEASE.zip"
-]
-
-download_and_extract_s3_files(s3_urls, clone_path)
-print(f"Files downloaded and extracted to {clone_path}")
+download_and_extract_s3_files(S3_DEPENDENCIES_URLS, FISHSTICK_PATH)
+print(f"Files downloaded and extracted to {FISHSTICK_PATH}")
